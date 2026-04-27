2332e7fe98fcfa82c7c0e68b11bb7eeebbf2fcda
yvxrick <riratsey@gmail.com>
2026-04-27 15:38:08 -0400

    Retrait du dossier de Dimitru

--- Projet03-WServeur-master/app/functions/page_is_active.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Function for dynamic navbars.
- */
-function page_active($page, $current) {
-    return $current === $page ? 'active' : '';
-}
--- Projet03-WServeur-master/app/start/init_env.php
+++
@@ -1,4 +0,0 @@
-<?php
-require "../phpdotenv/vendor/autoload.php";
-$dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
-$dotenv->load();
